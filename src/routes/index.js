6bcd92c08a7a6c1482a73adcc3f9f510c982edc6
axpronnier <axpronnier@a112pc25L.local.isima.fr>
2021-10-11 20:21:12 +0200

    fix index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,6 @@ import healthRouter from "./healthRouter"
 const router = express.Router()
 
 router.use("/health", healthRouter)
-// Add new routers above
+router.use("/games", gameRouter)
 
 export default router
