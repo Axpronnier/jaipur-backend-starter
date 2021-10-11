1df76ee6416bc0ebdff8ec370d96d3a09dac2064
axpronnier <axpronnier@a112pc25L.local.isima.fr>
2021-10-11 20:36:37 +0200

    fix export gamerouter.js

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -2,6 +2,7 @@ import express from "express"
 import * as gameService from "../services/gameService"
 
 const router = express.Router()
+
 // Listen to POST /games
 router.post("/", function (req, res) {
   if (!req.body.name) {
@@ -10,3 +11,4 @@ router.post("/", function (req, res) {
   const newGame = gameService.createGame(req.body.name)
   res.status(201).json(newGame)
 })
+export default router
